fd83483cb003bfa852df03f900c99fd7e6e82d66
mohdasif-02 <96809993+mohdasif-02@users.noreply.github.com>
2025-07-16 18:06:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Cloud Run + GitHub Actions!"
+    return "🚀 Hello from Cloud Run + GitHub Actions + Browser!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
